f0f964a7e1d14d7bb95f7201592cb75915ad0441
joaodevlopes <joaodevlopes@gmail.com>
2025-04-11 00:13:12 -0300

    Add project files.

---
+++ Poo1/Cake.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poo1
+{
+    public class Cake
+    {
+        //Atributos
+        public int Id;
+        public string Name;
+        public string Description;
+        public decimal Value;
+        public bool IsFilling;
+
+    }
+}
